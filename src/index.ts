fb92c073164a8fe2d8a48d2a5ace9d1ecc7aaa2c
Alon Diament <alondmnt@users.noreply.github.com>
2022-11-07 22:04:43 +0200

    new: fullwidth & halfwidth commands

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,19 @@ import { titleCase } from 'title-case';
 
 async function apply_case(case_type: string) {
 	let text = await joplin.commands.execute('selectedText');
-	text = text.toLowerCase();
 
 	if (case_type == 'upper') {
 		text = text.toUpperCase();
+	} else if (case_type == 'lower') {
+		text = text.toLowerCase();
 	} else if (case_type == 'title') {
 		text = toTitleCase(text);
 	} else if (case_type == 'sentence') {
 		text = toSentenceCase(text);
+	} else if (case_type == 'fullwidth') {
+		text = toFullWidth(text);
+	} else if (case_type == 'halfwidth') {
+		text = toHalfWidth(text);
 	}
 
 	await joplin.commands.execute('replaceSelection', text);
@@ -29,6 +34,18 @@ function toSentenceCase(text: string): string {
 		/([a-zA-Z]+[^.!?:\n]+[.!?:]*[\s]*)/g,
 		(match: string, offset: number) => match[0].toUpperCase() + match.slice(1)
 	);
+}
+
+function toFullWidth(text: string): string {
+	return text.replace(/([a-zA-Z0-9])/g, (match: string, offset: number) => {
+		return String.fromCharCode(match.charCodeAt(0) + 0xfee0);
+	});
+}
+
+function toHalfWidth(text: string): string {
+	return text.replace(/([！-～])/g, (match: string, offset: number) => {
+		return String.fromCharCode(match.charCodeAt(0) - 0xfee0);
+	});
 }
 
 joplin.plugins.register({
@@ -61,6 +78,20 @@ joplin.plugins.register({
 				apply_case('sentence');
 			}
 		});
+		joplin.commands.register({
+			name: 'suitcase.fullwidth',
+			label: 'ｆｕｌｌｗｉｄｔｈ',
+			execute: async () => {
+				apply_case('fullwidth');
+			}
+		});
+		joplin.commands.register({
+			name: 'suitcase.halfwidth',
+			label: 'halfwidth',
+			execute: async () => {
+				apply_case('halfwidth');
+			}
+		});
 		await joplin.views.menus.create('suitcaseMenu', 'Capitalization', [
 			{
 			  label: 'lower case',
@@ -81,6 +112,16 @@ joplin.plugins.register({
 			  label: 'Sentence case',
 			  commandName: 'suitcase.sentence',
 			  accelerator: 'CmdOrCtrl+Alt+Shift+S',
+			},
+			{
+			  label: 'ｆｕｌｌｗｉｄｔｈ',
+			  commandName: 'suitcase.fullwidth',
+			  accelerator: 'CmdOrCtrl+Alt+Shift+F',
+			},
+			{
+			  label: 'halfwidth',
+			  commandName: 'suitcase.halfwidth',
+			  accelerator: 'CmdOrCtrl+Alt+Shift+H',
 			},
 		  ], MenuItemLocation.Edit);
 	},
